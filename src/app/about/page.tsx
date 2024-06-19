244991c55c623e3de2fb31699cd92ecc56e92ab6
Rick Kock <rit.kock@gmail.com>
2024-06-19 21:24:08 +0200

    chore: initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutUs() {
+  return (
+    <main>
+      <div className="bg-stone-100 text-slate-500 flex justify-center min-h-80">
+        <h1>About Us</h1>
+        <p>Lorum ipsum or some shit like that...</p>
+      </div>
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Nav, NavLink } from "@/components/Nav";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav>
+          <NavLink href={"/"}>Home</NavLink>
+          <NavLink href={"/catalog"}>Catalog</NavLink>
+          <NavLink href={"/blog"}>Blog</NavLink>
+          <NavLink href={"/about"}>About us</NavLink>
+          <NavLink href={"/contact"}>Contact</NavLink>
+        </Nav>
+        {children}
+        <footer className="bg-stone-800 text-stone-50 p-4">
+          This is the footer
+        </footer>
+      </body>
     </html>
   );
 }
